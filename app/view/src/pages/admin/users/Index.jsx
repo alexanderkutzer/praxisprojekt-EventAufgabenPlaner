02c0f471c62131cd6dbbcc1af0312126039ae547
Sima21 <af.akhtari@yahoo.de>
2024-10-18 16:25:20 +0200

    ModalFenster for User

--- app/view/src/pages/admin/users/Index.jsx
+++ app/view/src/pages/admin/users/Index.jsx
@@ -3,16 +3,19 @@ import UsersList from "./components/UsersList";
 import UserEdit from "./components/UserEdit";
 
 function PageUsers() {
+    const [users, setUsers] = React.useState([]);
+
     const [selectedUser, setSelectedUser] = React.useState(null);
+
     return (
         <div className="flex flex-col">
             <div>Users</div>
             <div className="flex flex-row gap-2">
                 <div className="w-1/2 rounded-md border border-black p-1">
-                    <UsersList selectedUser={selectedUser} setSelectedUser={setSelectedUser}></UsersList>
+                    <UsersList selectedUser={selectedUser} setSelectedUser={setSelectedUser} users={users} setUsers={setUsers}></UsersList>
                 </div>
                 <div className="w-1/2 rounded-md border border-black p-1">
-                    <UserEdit selectedUser={selectedUser} setSelectedUser={setSelectedUser}></UserEdit>
+                    <UserEdit selectedUser={selectedUser} setSelectedUser={setSelectedUser} users={users} setUsers={setUsers}></UserEdit>
                 </div>
             </div>
         </div>
