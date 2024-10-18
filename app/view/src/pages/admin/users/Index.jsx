4b2daca94bd068d418d9f56e16ee778ac1caffbc
Sima21 <af.akhtari@yahoo.de>
2024-10-18 10:59:35 +0200

    AdminUser

--- app/view/src/pages/admin/users/Index.jsx
+++ app/view/src/pages/admin/users/Index.jsx
@@ -3,15 +3,16 @@ import UsersList from "./components/UsersList";
 import UserEdit from "./components/UserEdit";
 
 function PageUsers() {
+    const [selectedUser, setSelectedUser] = React.useState(null);
     return (
         <div className="flex flex-col">
             <div>Users</div>
             <div className="flex flex-row gap-2">
                 <div className="w-1/2 rounded-md border border-black p-1">
-                    <UsersList></UsersList>
+                    <UsersList selectedUser={selectedUser} setSelectedUser={setSelectedUser}></UsersList>
                 </div>
                 <div className="w-1/2 rounded-md border border-black p-1">
-                    <UserEdit></UserEdit>
+                    <UserEdit selectedUser={selectedUser} setSelectedUser={setSelectedUser}></UserEdit>
                 </div>
             </div>
         </div>
